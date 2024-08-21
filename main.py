08a435403ab09700c65b1be7c985991ff5480577
Jordan Ward <jordancward@gmail.com>
2024-08-21 13:50:39 -0600

    Picture from camera or uploaded image -> grayscale

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from PIL import Image
 
+uploaded_image = st.file_uploader("Upload Image")
+
 with st.expander("Start Camera"):
     # start the camera
     camera_image = st.camera_input("Camera")
@@ -16,3 +18,14 @@ if camera_image:
 
     # render the grayscale image on the webpage
     st.image(gray_img)
+
+
+if uploaded_image:
+    # create a pillow (PIL) image instance
+    img2 = Image.open(uploaded_image)
+
+    # convert the pillow (PIL) image to grayscale
+    gray_img2 = img2.convert("L")
+
+    # render the grayscale image on the webpage
+    st.image(gray_img2)
